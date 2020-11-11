b6b16dbfa8967bc3b6a5940c9a84a8cdd8f03745
bluebeacon8402 <crsorge@icloud.com>
2020-11-11 11:52:35 -0500

    new exercise

---
+++ PracticeLinq1/PracticeLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var videoGames = new List<string>();
+
+            videoGames.Add("PacMan");
+            videoGames.Add("SuperMarioBros");
+            videoGames.Add("MorrowWind");
+            videoGames.Add("Sonic");
+
+            var result = videoGames.OrderByDescending(Sort => Sort.Length);
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
